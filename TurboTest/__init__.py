27b70fb2e8438a75ef0191f1534dad362464d494
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-02 07:58:02 -0700

    0.2.0b2 testing

--- TurboTest/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from mykit.kit.time import TimeFmt
-
-
-def main():
-
-    CWD = os.getcwd()
-    print(12312312312123)
--- TurboTest/__main__.py
+++ TurboTest/__main__.py
@@ -1,2 +1,2 @@
-from . import main
-if __name__=='__main__':main()
+from .runner import run
+if __name__=='__main__':run()
