67a98e49b76c986f8e5de12c53267fef3a83f50b
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-05 00:43:51 -0700

    0.13.0b testing

--- TurboTest/runner/__init__.py
+++ TurboTest/runner/__init__.py
@@ -28,7 +28,15 @@ class R:  # Runtime
     details = []
 
 
-def run_tester(module, function):
+# def run_tester(module, function):
+def run_tester(__TT_module, __TT_function):
+    ## Docs: the "__TT_" prefix is used to make sure these function
+    ##       args' variables will not conflict with the called function
+    ##       `__TT_function` inside `locals()`.
+    ## Note: the "__TT_" prefix is actually double checker, even though
+    ##       `__TT_function` will never let in the functions that start
+    ##       with an underscore (_), since functions that start with an
+    ##       underscore are not considered as testers.
 
     # def wrap():
     #     print(f"locals before: {locals()}")
@@ -38,12 +46,12 @@ def run_tester(module, function):
     # vvvvvvvvvvvvvvvvvvv wrapping as shown above still allows access to `module` and `function` within the `locals()` scope
 
     # exec(f"{function}()")  # Testing purposes
-    print(f"nChem: {repr(Container.nChemical)}")
-    print(f"locals before: {locals()}")
-    exec(f'from {module} import {function}')
-    print(f"locals after : {locals()}")
-    exec(f"{function}()")
-    print(f"nChem: {repr(Container.nChemical)}")
+    # print(f"nChem: {repr(Container.nChemical)}")
+    # print(f"locals before: {locals()}")
+    exec(f'from {__TT_module} import {__TT_function}')
+    # print(f"locals after : {locals()}")
+    exec(f"{__TT_function}()")
+    # print(f"nChem: {repr(Container.nChemical)}")
 
 def recur(dir_pth):
     
@@ -61,23 +69,23 @@ def recur(dir_pth):
                             f = res.group('name')
                             if not f.startswith('_'):
                                 funcs.append(f)
-                    print(f'funcs={funcs}  at {repr(pth)}')
+                    # print(f'funcs={funcs}  at {repr(pth)}')
 
-                    print(f"DEBUG: pth: {pth}")
+                    # print(f"DEBUG: pth: {pth}")
                     module_name_a = os.path.relpath(pth, C.CWD)
                     module_name_b = os.path.relpath(pth, C.CWD).split(os.sep)
                     module_name_c = '.'.join(os.path.relpath(pth, C.CWD).split(os.sep))
                     module_name = '.'.join(os.path.relpath(pth, C.CWD).split(os.sep))[:-3]
-                    print(f"DEBUG: module_name_a: {module_name_a}")
-                    print(f"DEBUG: module_name_b: {module_name_b}")
-                    print(f"DEBUG: module_name_c: {module_name_c}")
-                    print(f"DEBUG: module_name  : {module_name}")
+                    # print(f"DEBUG: module_name_a: {module_name_a}")
+                    # print(f"DEBUG: module_name_b: {module_name_b}")
+                    # print(f"DEBUG: module_name_c: {module_name_c}")
+                    # print(f"DEBUG: module_name  : {module_name}")
                     
                     for ff in funcs:
                         tester_t0 = time.time()
                         run_tester(module_name, ff)
                         tester_t1 = time.time() - tester_t0
-                        print(f'[{TimeFmt.hour()}]: PASS: {module_name}: {ff}  ({Container.nChemical}🧪|{time_quick(tester_t1)})')
+                        print(f'[{TimeFmt.hour()}]: PASS: {module_name[:-5]}: {ff.replace("_", " ")}  ({Container.nChemical}🧪|{time_quick(tester_t1)})')
                         Container.clear_chemical()
 
                     ## Make sure there is no duplication of tester names (each tester must have a unique name)
